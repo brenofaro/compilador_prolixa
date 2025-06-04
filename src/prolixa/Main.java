e21d7e6858ee842adf3e81db529471c8ff530d91
Brenno Faro <brenofaro97@hotmail.com>
2025-06-04 12:24:23 -0300

    Innitial Commit

---
+++ src/prolixa/Main.java
@@ -0,0 +1,29 @@
+package prolixa;
+import prolixa.lexer.*;
+import prolixa.node.*;
+import java.io.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            String arquivo = "codFonte/codFonte.prol";
+
+            Lexer lexer =
+                    new Lexer(
+                            new PushbackReader(
+                                    new FileReader(arquivo), 1024));
+            Token token;
+            while(!((token = lexer.next()) instanceof EOF)) {
+                System.out.println(token.getClass());
+                System.out.println(" ( "+token.toString()+")");
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
